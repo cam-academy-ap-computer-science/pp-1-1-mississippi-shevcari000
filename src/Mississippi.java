e61ad7627bfc779808efd8e0ab4a6709837bdd34
shevcari000 <shevcari000@CAM-LAB-27.bgsd.k12.wa.us>
2019-09-13 07:42:32 -0700

    Work in Progress

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -3,7 +3,23 @@ public class Mississippi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DrawM();
+		DrawI();
 
 	}
-
+    public static void DrawM () {
+    	System.out.println ("M     M");
+    	System.out.println ("MM   MM");
+    	System.out.println ("M M M M");
+    	System.out.println ("M  M  M");
+    	System.out.println ("M     M");
+    	System.out.println ("M     M");
+    	System.out.println ("M     M");
+    }
+   public static void DramI () {
+	    System.out.println ("IIIII");
+	    System.out.println ("  I  ");
+	    System.out.println ("  I  ");
+   } 
 }
+
